08550ee056d32de0917c32b74f7268ac211d9477
George Jeng <gjenga@icloud.com>
2022-05-03 15:06:08 -0700

    Created side and topbar layout
    Co-authored-by: Will Chu <williamchu9@gmail.com>
    Co-authored-by: Steven Nguyen <sateeven@gmail.com>
    Co-authored-by: George Jeng <gjenga@icloud.com>
    Co-authored-by: Rankin Draa <rankindraa@gmail.com>

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import '../bundle/output.css';
+import DashboardContainer from '../client/containers/DashboardContainer';
 
 export default function App() {
 
   return (
-    <div className="text-indigo-500 text-sm">
-      HELLO TAILWIND
+    <div>
+      <DashboardContainer />
       <Router>
         <div>
           <Routes>
